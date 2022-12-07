3cc6623b260ad19b694c9a3cf7a717e186224dd8
Sunny Sahu <X2SSAHU@southernco.com>
2022-12-07 15:24:42 +0530

    Add project files.

---
+++ FileUploadAPI/App_Start/WebApiConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http.Cors;
+using System.Web.Http;
+
+namespace FileUploadAPI
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            var cors = new EnableCorsAttribute("*", "*", "*"); //origins,headers,methods  
+            config.EnableCors(cors);
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
